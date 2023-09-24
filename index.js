d535d27437792efe8c80fa36176fb3453c0bc6fb
Aleksandr Platcev <a.platcev@ya.ru>
2023-09-25 00:42:12 +0300

    q

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socketChat = require('./websocket/Chat');
 // const socketInstance = require('./websocket/InstanceManager');
 
 const server = app.listen(3000, () => {
-  console.log(`App is running on ${port} http://localhost:3000`);
+  console.log('App is running on http://localhost:3000');
 });
 socketChat(server);
 // socketInstance(server);
