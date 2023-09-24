c7ac03143f776086b66bb485056eb16d8066be83
Aleksandr Platcev <a.platcev@ya.ru>
2023-09-24 23:53:24 +0300

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 // const socketInstance = require('./websocket/InstanceManager');
 
 const server = app.listen(port, () => {
-  console.log(```App is running on ${port} http://localhost:3000```);
+  console.log(`App is running on ${port} http://localhost:3000`);
 });
 socketChat(server);
 // socketInstance(server);
