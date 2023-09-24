733b02d606e84532544ed2dd1d3a134c668ee771
Aleksandr Platcev <a.platcev@ya.ru>
2023-09-24 23:16:50 +0300

    add port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const socketChat = require('./websocket/Chat');
+const port = process.env.PORT || 3000;
 // const socketInstance = require('./websocket/InstanceManager');
 
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
   console.log('App is running on http://localhost:3000');
 });
 socketChat(server);
