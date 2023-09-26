6b34042097892004106e4fa3659deac1f990f567
Aleksandr Platcev <a.platcev@ya.ru>
2023-09-26 11:03:17 +0300

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const socketChat = require('./websocket/Chat');
 const socketInstance = require('./websocket/InstanceManager');
 
-const server = app.listen(80, () => {
+const server = app.listen(3000, () => {
   console.log('App is running on http://localhost:3000');
 });
 socketChat(server);
