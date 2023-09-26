eb4b6020778231cd06b19acb866d11a2fc8c0623
Aleksandr Platcev <a.platcev@ya.ru>
2023-09-26 10:31:57 +0300

    add cors

--- websocket/Chat.js
+++ websocket/Chat.js
@@ -5,7 +5,7 @@ const { chatMembers } = require('../data/db');
 function chat(server) {
   const io = socketIO(server, {
     cors: {
-      origin: ['http://localhost:8080', '85.172.39.83'],
+      origin: ['http://localhost:8080', 'https://npukojiucm.github.io'],
       method: ['GET', 'POST']
     },
   });
